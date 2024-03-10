929b20adce7675dddaf4221c010fd74f7be3421c
Suedy Mud <suedymud@hotmail.com>
2024-03-10 11:36:30 +0100

    restoration des parties supprimer

---
+++ inventaire-backend/src/test/java/be/eafcuccle/tfe/inventairebackend/InventaireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.eafcuccle.tfe.inventairebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventaireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
