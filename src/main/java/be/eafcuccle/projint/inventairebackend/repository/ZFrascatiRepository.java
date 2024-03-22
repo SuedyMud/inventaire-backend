c9c7e8c4e295f1e17ddde2fbb1a8515fecee660a
Suedy Mud <suedymud@hotmail.com>
2024-03-22 14:47:04 +0100

    Authentification Backend (avec Auth0)

--- src/main/java/be/eafcuccle/projint/inventairebackend/repository/ZFrascatiRepository.java
+++ src/main/java/be/eafcuccle/projint/inventairebackend/repository/ZFrascatiRepository.java
@@ -9,8 +9,12 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface ZFrascatiRepository extends JpaRepository<ZFrascati, String> {
-    @Query("select zf from ZFrascati zf")
-    Page<ZFrascati> findZFrascati(Pageable pageable);
+    /*@Query("select zf from ZFrascati zf")
+    Page<ZFrascati> findZFrascati(Pageable pageable);*/
+
+    Page<ZFrascati> findAll(Pageable pageable);
+
 
     List<ZFrascati> findAll();
+
 }
