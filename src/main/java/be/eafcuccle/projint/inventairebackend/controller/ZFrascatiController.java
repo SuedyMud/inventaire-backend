92cd729e6be67151c548926c3628bbbd0158944b
Suedy Mud <suedymud@hotmail.com>
2024-06-08 20:19:25 +0200

    ajout des permissions pour les frascatis

--- src/main/java/be/eafcuccle/projint/inventairebackend/controller/ZFrascatiController.java
+++ src/main/java/be/eafcuccle/projint/inventairebackend/controller/ZFrascatiController.java
@@ -4,9 +4,12 @@ import be.eafcuccle.projint.inventairebackend.model.ZFrascati; // Modification d
 import be.eafcuccle.projint.inventairebackend.repository.ZFrascatiRepository; // Modification de l'import
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -14,85 +17,118 @@ import java.net.URI;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/zfrascati") // Modification du mapping
+@RequestMapping("/api/zfrascati")
 public class ZFrascatiController {
 
     private final Logger logger = LoggerFactory.getLogger(ZFrascatiController.class);
 
-    private final ZFrascatiRepository zfrascatiRepository; // Modification du nom de la variable
-    private int currentId = 1; // Initialisation du compteur
+    @Autowired
+    private ZFrascatiRepository zfrascatiRepository;
+    private int currentId = 1;
 
-    public ZFrascatiController(ZFrascatiRepository zfrascatiRepository) { // Modification du nom du paramètre
-        this.zfrascatiRepository = zfrascatiRepository; // Modification de l'assignation
-    }
+
     @PostMapping("/ajouter")
-    public ResponseEntity<?> ajouterFrascati(@RequestBody ZFrascati zfrascati, UriComponentsBuilder builder) {
-        logger.info("Tentative d'ajout d'un nouveau ZFrascati avec l'ID : " + zfrascati.getIdfrascati());
+    public ResponseEntity<?> ajouterFrascati(@RequestBody ZFrascati zfrascati, UriComponentsBuilder builder, Authentication authentication) {
+        if(hasAuthority(authentication, "SCOPE_write:information")){
+            logger.info("Tentative d'ajout d'un nouveau ZFrascati avec l'ID : " + zfrascati.getIdfrascati());
 
-        // Affecter un nouvel ID unique en utilisant UUID
-        zfrascati.setIdfrascati(String.valueOf(currentId++)); // Conversion de l'ID en chaîne de caractères et incrémentation
 
-        zfrascatiRepository.save(zfrascati);
+            zfrascati.setIdfrascati(String.valueOf(currentId++)); // Conversion de l'ID en chaîne de caractères et incrémentation
 
-        URI localisation = builder.path("/api/zfrascati/{id}").buildAndExpand(zfrascati.getIdfrascati()).toUri();
-        return ResponseEntity.created(localisation).body(zfrascati);
+            zfrascatiRepository.save(zfrascati);
+
+            URI localisation = builder.path("/api/zfrascati/{id}").buildAndExpand(zfrascati.getIdfrascati()).toUri();
+            return ResponseEntity.created(localisation).body(zfrascati);
+        }else{
+            logger.debug("Accès refusé ! L'utilisateur n'a pas la permission d'ajouter une nouvelle frascati.");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
     }
 
     @GetMapping("/liste")
-    public Page<ZFrascati> listeFrascati(Pageable pageable) { // Modification du nom de la méthode et du type de retour
-        logger.info("Tentative de récupération d'une liste paginée de Frascati."); // Modification du nom de l'objet
-        return zfrascatiRepository.findAll(pageable); // Modification du nom de la variable
+    public Page<ZFrascati> listeFrascati(Pageable pageable, Authentication authentication) {
+        if(hasAuthority(authentication, "SCOPE_read:information")){
+            logger.info("Tentative de récupération d'une liste paginée de frascati."); // Modification du nom de l'objet
+            return zfrascatiRepository.findAll(pageable); // Modification du nom de la variable
+        }else{
+            logger.debug("Accès refusé ! L'utilisateur n'a pas la permission d'accéder à la liste des frascatis.");
+            return null;
+        }
+
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> supprimerFrascati(@PathVariable String id) {
-        logger.info("Tentative de suppression d'un Frascati avec l'ID : " + id);
-        zfrascatiRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<?> supprimerFrascati(@PathVariable String id, Authentication authentication) {
+        if (hasAuthority(authentication, "SCOPE_write:information")) {
+            logger.info("Tentative de suppression d'un frascatis avec l'ID : " + id);
+            zfrascatiRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+
+        }else{
+            logger.debug("Accès refusé ! L'utilisateur n'a pas la permission de supprimer une frascati.");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
     }
 
     @GetMapping("/{id}")
     @ResponseBody
-    public ResponseEntity<ZFrascati> detailZFrascati(@PathVariable String id) {
-        logger.info("Tentative de récupération du détail d'un ZFrascati avec l'ID : " + id);
+    public ResponseEntity<ZFrascati> detailZFrascati(@PathVariable String id, Authentication authentication) {
+        if (hasAuthority(authentication, "SCOPE_read:information")) {
+            logger.info("Tentative de récupération du détail d'un frascatis avec l'ID : " + id);
 
-        // Récupération du ZFrascati correspondant à l'ID
-        Optional<ZFrascati> optionalZFrascati = zfrascatiRepository.findById(id);
+            // Récupération du ZFrascati correspondant à l'ID
+            Optional<ZFrascati> optionalZFrascati = zfrascatiRepository.findById(id);
 
-        // Vérification de l'existence du ZFrascati
-        if (optionalZFrascati.isPresent()) {
-            logger.debug("Succès du détail du ZFrascati avec l'ID : " + id);
-            return ResponseEntity.ok(optionalZFrascati.get());
-        } else {
-            logger.debug("ZFrascati introuvable avec l'ID : " + id);
-            return ResponseEntity.notFound().build();
+            // Vérification de l'existence du ZFrascati
+            if (optionalZFrascati.isPresent()) {
+                logger.debug("Succès du détail du ZFrascati avec l'ID : " + id);
+                return ResponseEntity.ok(optionalZFrascati.get());
+            } else {
+                logger.debug("ZFrascati introuvable avec l'ID : " + id);
+                return ResponseEntity.notFound().build();
+            }
+        }else{
+            logger.debug("Accès refusé ! L'utilisateur n'a pas la permission d'afficher les détails d'une frascati.");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
+
     }
 
 
     @PutMapping("/{id}")
-    public ResponseEntity<ZFrascati> modifierFrascati(@PathVariable String id, @RequestBody ZFrascati updatedFrascati) {
-        logger.info("Tentative de mise à jour d'un Frascati avec l'ID : " + id);
+    public ResponseEntity<ZFrascati> modifierFrascati(@PathVariable String id, @RequestBody ZFrascati updatedFrascati, Authentication authentication) {
+        if (hasAuthority(authentication, "SCOPE_read:information")) {
+            logger.info("Tentative de mise à jour d'un frascati avec l'ID : " + id);
 
-        Optional<ZFrascati> optionalFrascati = zfrascatiRepository.findById(id);
-        if (optionalFrascati.isPresent()) {
-            ZFrascati existingFrascati = optionalFrascati.get();
+            Optional<ZFrascati> optionalFrascati = zfrascatiRepository.findById(id);
+            if (optionalFrascati.isPresent()) {
+                ZFrascati existingFrascati = optionalFrascati.get();
 
-            // Mettre à jour les champs modifiables
-            existingFrascati.setFrascati(updatedFrascati.getFrascati());
-            existingFrascati.setFrascatiUK(updatedFrascati.getFrascatiUK());
-            existingFrascati.setDescription(updatedFrascati.getDescription());
-            existingFrascati.setDescriptionUK(updatedFrascati.getDescriptionUK());
-            existingFrascati.setRefgrdiscip(updatedFrascati.getRefgrdiscip());
-            existingFrascati.setOrdre(updatedFrascati.getOrdre());
+                // Mettre à jour les champs modifiables
+                existingFrascati.setFrascati(updatedFrascati.getFrascati());
+                existingFrascati.setFrascatiUK(updatedFrascati.getFrascatiUK());
+                existingFrascati.setDescription(updatedFrascati.getDescription());
+                existingFrascati.setDescriptionUK(updatedFrascati.getDescriptionUK());
+                existingFrascati.setRefgrdiscip(updatedFrascati.getRefgrdiscip());
+                existingFrascati.setOrdre(updatedFrascati.getOrdre());
 
 
-            zfrascatiRepository.save(existingFrascati);
-            logger.debug("Succès de la mise à jour du Frascati");
-            return ResponseEntity.ok(existingFrascati);
+                zfrascatiRepository.save(existingFrascati);
+                logger.debug("Succès de la mise à jour du Frascati");
+                return ResponseEntity.ok(existingFrascati);
+            } else {
+                logger.debug("Échec, Frascati introuvable");
+                return ResponseEntity.notFound().build();
+            }
         } else {
-            logger.debug("Échec, Frascati introuvable");
-            return ResponseEntity.notFound().build();
+            logger.debug("Accès refusé ! L'utilisateur n'a pas la permission de mettre à jour une frascati.");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
+
+    }
+
+    public static boolean hasAuthority(Authentication authentication, String expectedAuthority) {
+        return authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals(expectedAuthority));
     }
 }
